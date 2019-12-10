56284a8b60642d7133f18c38a9ba70d545a04a20
Vladimir Kamuz <v.kamuz@gmail.com>
2019-12-11 00:03:20 +0200

    First example with WP_Query

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php get_header(); ?>
 	<div class="container">
 		<h1 class="title"><?php bloginfo('name') ?></h1>
-		<?php if(have_posts()): ?>
+		<?php
+		$args = array(
+			'post_type' => 'post',
+			'posts_per_page' => 5
+		);
+		$query = new WP_Query($args);
+		?>
+		<?php if($query->have_posts()): ?>
 			<ul>
-				<?php while(have_posts()) : the_post(); ?>
+				<?php while($query->have_posts()) : $query->the_post(); ?>
 					<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
 				<?php endwhile;?>
 			</ul>
+		<?php else: ?>
+			<?php echo __('Pages not found yet, sorry!', 'simple') ?>
 		<?php endif; ?>
+		<?php wp_reset_query(); ?>
 	</div>
 <?php get_footer(); ?>
