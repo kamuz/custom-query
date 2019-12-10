37ee337b920255e322de43567c63bb710cf6ce35
Vladimir Kamuz <v.kamuz@gmail.com>
2019-12-10 22:43:24 +0200

    Disable updates

--- functions.php
+++ functions.php
@@ -17,3 +17,17 @@ if (function_exists('add_theme_support')) {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 }
+
+/**
+ * Disable updates
+ */
+remove_action('load-update-core.php', 'wp_update_plugins');
+add_filter('pre_site_transient_update_plugins', '__return_null');
+
+function remove_core_updates(){
+	global $wp_version;
+	return(object) array('last_checked'=> time(), 'version_checked'=> $wp_version);
+}
+add_filter('pre_site_transient_update_core', 'remove_core_updates');
+add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
+add_filter('pre_site_transient_update_themes', 'remove_core_updates');
