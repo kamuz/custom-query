0ab531a6c1c681f58adbe3d3cd78d5bd04f9a85a
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-05-14 23:15:49 +0300

    Sort by meta fields

--- index.php
+++ index.php
@@ -4,38 +4,21 @@
 		<?php
 		$args  = array(
 			'post_type'      => 'post',
-			'posts_per_page' => 1,
+			'posts_per_page' => 100,
+			'meta_key'       => 'views',
+			'orderby'        => 'meta_value_num',
+			'order'          => 'DESC',
 		);
 		$query = new WP_Query( $args );
 		?>
 		<?php if ( $query->have_posts() ) : ?>
-			<div class="featured">
+			<div>
 				<?php while ( $query->have_posts() ) : ?>
 					<?php $query->the_post(); ?>
-					<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+					<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php echo ! empty( get_field( 'views' ) ) ? '(' . esc_html( get_field( 'views' ) . ')' ) : '(0)'; ?> <?php echo ! empty( get_field( 'source' ) ) ? esc_html( ' — ' . get_field( 'source' ) ) : ''; ?> </li>
 					<?php $current_post = $post->ID; ?>
 				<?php endwhile; ?>
 			</div>
-		<?php else : ?>
-			<?php echo esc_html__( 'Pages not found yet, sorry!', 'simple' ); ?>
-		<?php endif; ?>
-		<?php wp_reset_postdata(); ?>
-		<?php
-		$args = array(
-			'post_type'      => 'post',
-			'posts_per_page' => 4,
-			'post__not_in'   => array( $current_post ),
-
-		);
-		$query = new WP_Query( $args );
-		?>
-		<?php if ( $query->have_posts() ) : ?>
-			<ul>
-				<?php while ( $query->have_posts() ) : ?>
-					<?php $query->the_post(); ?>
-					<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-				<?php endwhile; ?>
-			</ul>
 		<?php else : ?>
 			<?php echo esc_html__( 'Pages not found yet, sorry!', 'simple' ); ?>
 		<?php endif; ?>
